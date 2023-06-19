20c53fd4adf3e279c0eed9f0ecdb89e0a3b68e3a
ivan silvio <ivan.sl@hotmail.com>
2023-06-18 23:07:43 -0300

    Update ECE.sh

--- ECE.sh
+++ ECE.sh
@@ -136,3 +136,5 @@ su - elastic
 
 bash <(curl -fsSL https://download.elastic.co/cloud/elastic-cloud-enterprise.sh) install --force --availability-zone POC-01 --memory-settings '{"runner":{"xms":"4G","xmx":"4G"},"allocator":{"xms":"4G","xmx":"4G"},"zookeeper":{"xms":"4G","xmx":"4G"},"director":{"xms":"4G","xmx":"4G"},"constructor":{"xms":"4G","xmx":"4G"},"admin-console":{"xms":"4G","xmx":"4G"}}'
 
+
+sudo rm -rf /tmp/ECE.sh
